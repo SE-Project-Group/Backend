d7606e3f1af6062c86f32e6a1557fd8e11ea515e
Wang Tao <thor.wang97@outlook.com>
2017-07-14 11:46:07 +0800

    get picture url from oss

--- track/src/main/java/action/FeedAction.java
+++ track/src/main/java/action/FeedAction.java
@@ -1,10 +1,15 @@
 package action;
 
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.internal.ResponseParsers.ListImageStyleResponseParser;
 
 import model.Feed;
 import service.AppService;
@@ -16,6 +21,9 @@ public class FeedAction extends BaseAction{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private String feedId;
+	private int picCount;
+	
 	private AppService appService;
 	
 	public AppService getAppService() {
@@ -26,6 +34,16 @@ public class FeedAction extends BaseAction{
 		this.appService = appService;
 	}
 	
+	
+	public void setFeedId(String feedId) {
+		this.feedId = feedId;
+	}
+
+
+	public void setPicCount(int picCount) {
+		this.picCount = picCount;
+	}
+
 	public String getBestFeed(){
 		Date date=new Date(System.currentTimeMillis());
 		List<Feed> feeds=appService.getTodayFeedList(date);
@@ -43,6 +61,25 @@ public class FeedAction extends BaseAction{
 		}
 		request().setAttribute("feeds", res);
 		return SUCCESS;
+	}
+	
+	
+	public void getURL() throws Exception{
+		String accessKeyId = "LTAIdLRXuEkhtIYE";
+		String accessKeySecret = "bQ0VEJQkWXKwZ4NBlCMVe6YPVbuC4y";
+		String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
+		String bucket = "sjtutest";
+		java.util.Date expiration = new java.util.Date((new java.util.Date().getTime() + 3600 * 1000));
+		OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		
+		List<String> list = new ArrayList<>();
+		for(int i = 1; i <= picCount; i++){
+			String key = feedId + "_" + String.valueOf(i);
+			URL url = client.generatePresignedUrl(bucket, key, expiration);
+			list.add(url.toString());
+		}
+		String listStr = list.toString();
+		response().getWriter().write(listStr);
 	}
 
 	
