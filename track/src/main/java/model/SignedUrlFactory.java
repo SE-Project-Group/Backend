1f4a3629d38531d5b7142460665fc6bba7f985de
Wang Tao <thor.wang97@outlook.com>
2017-08-09 03:44:17 +0800

    查看大图调用接口

--- track/src/main/java/model/SignedUrlFactory.java
+++ track/src/main/java/model/SignedUrlFactory.java
@@ -16,8 +16,8 @@ public class SignedUrlFactory {
 	private final static String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
 	private final static String bucket = "sjtutest";
 	
+	// get thumbnail pic urls
 	public List<String> getPicUrls(String feedId, int picCount){
-		
 		Date expiration = new Date((new java.util.Date().getTime() + 3600 * 1000));
 		OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
 		List<String> list = new ArrayList<>();
@@ -36,11 +36,45 @@ public class SignedUrlFactory {
 		return list;
 	}
 	
+	// get big pic urls
+	public List<String> getBigPicUrls(String feedId, int picCount){
+		Date expiration = new Date((new java.util.Date().getTime() + 3600 * 1000));
+		OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		List<String> list = new ArrayList<>();
+		for(int i = 1; i <= picCount; i++){
+			String key = feedId + "_" + String.valueOf(i);
+			String style = "style/big_pic";
+			
+			GeneratePresignedUrlRequest req = new GeneratePresignedUrlRequest(bucket, key, HttpMethod.GET);
+		    req.setExpiration(expiration);
+		    req.setProcess(style);
+		    
+			URL url = client.generatePresignedUrl(req);
+			list.add(url.toString());
+		}
+		
+		return list;
+	}
+	
 	public String getPortraitUrl(int user_id){
 		Date expiration = new Date((new java.util.Date().getTime() + 3600 * 1000));
 		OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
 		String key = user_id + "_portrait";
 		String style = "style/thumbnail_portrait";
+		
+		GeneratePresignedUrlRequest req = new GeneratePresignedUrlRequest(bucket, key, HttpMethod.GET);
+	    req.setExpiration(expiration);
+	    req.setProcess(style);
+	    
+		URL url = client.generatePresignedUrl(req);
+		return url.toString();
+	}
+	
+	public String getBigPortraitUrl(int user_id){
+		Date expiration = new Date((new java.util.Date().getTime() + 3600 * 1000));
+		OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		String key = user_id + "_portrait";
+		String style = "style/big_pic";
 		
 		GeneratePresignedUrlRequest req = new GeneratePresignedUrlRequest(bucket, key, HttpMethod.GET);
 	    req.setExpiration(expiration);
